7574282cfbc9e96dad62f1e4e2b017b71bc98cc6
Puripant <puripant@gmail.com>
2021-01-01 22:08:22 +0700

    Adjust x scale and some link drawing

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const margins = { top: 20, bottom: 20, left: 20, right: 100 }
+const width = 1000
 const color_key = (key) => key.split(" ")[0]
 
 d3.csv("data.csv").then((data) => {
@@ -5,13 +7,20 @@ d3.csv("colors.csv").then((colors) => {
   let faculties = colors.map(d => d.faculty)
   colors = colors.map(d => d.rgb)
 
-  let color_scale = d3.scaleOrdinal(faculties, colors).unknown("#000000");
+  let color_scale = d3.scaleOrdinal(faculties, colors)
+    .unknown("#000000");
+  let x_scale = d3.scaleLinear(d3.extent(data, d => +d.year), [margins.left, margins.left + width])
+    .unknown(margins.left)
 
   data = data.filter(d => d.type != "context")
   data.sort((a, b) => +a.year - +b.year)
   data = Array.from(d3.group(data, d => d.year), ([key, value]) => value)
   let levels = data.map(year => {
-    return Array.from(d3.group(year, d => d.to), ([key, value]) => ({id: value[0].to, parents: value.map(d => d.from)}))
+    return Array.from(d3.group(year, d => d.to), ([key, value]) => ({
+      id: value[0].to, 
+      parents: value.map(d => d.from),
+      year: value[0].year
+    }))
   })
 
   // Adapted from https://observablehq.com/@nitaku/tangled-tree-visualization-ii
@@ -26,8 +35,6 @@ d3.csv("colors.csv").then((colors) => {
   nodes.forEach(d => {
     d.parents = (d.parents === undefined || d.parents[0] === "" ? [] : d.parents).map(p => nodes_index[p])
   })
-
-  console.log(levels)
 
   // precompute bundles
   levels.forEach((l, i) => {
@@ -54,7 +61,7 @@ d3.csv("colors.csv").then((colors) => {
     d.parents.forEach(p => links.push({source: d, bundle: d.bundle, target: p}))
   })
 
-  let bundles = levels.reduce( ((a,x) => a.concat(x.bundles)), [] )
+  let bundles = levels.reduce((a,x) => a.concat(x.bundles), [])
 
   // reverse pointer from parent to bundles
   bundles.forEach(b => b.parents.forEach(p => {
@@ -88,22 +95,22 @@ d3.csv("colors.csv").then((colors) => {
   // layout
   const padding = 8
   const node_height = 22
-  const node_width = 70
-  const bundle_width = 14
+  // const node_width = 70
+  // const bundle_width = 14
   const level_y_padding = 16
   const metro_d = 4
   const c = 16
-  const min_family_height = 16
+  // const min_family_height = 16
 
   nodes.forEach(n => n.height = (Math.max(1, n.bundles.length)-1)*metro_d)
 
-  let x_offset = padding
+  // let x_offset = padding
   let y_offset = padding
   levels.forEach(l => {
-    x_offset += l.bundles.length*bundle_width
+    // x_offset += l.bundles.length*bundle_width
     y_offset += level_y_padding
     l.forEach((n, i) => {
-      n.x = n.level*node_width + x_offset
+      n.x = x_scale(n.year) //n.level*node_width + x_offset
       n.y = node_height + y_offset + n.height/2
       
       y_offset += node_height + n.height
@@ -113,7 +120,7 @@ d3.csv("colors.csv").then((colors) => {
   let i = 0
   levels.forEach(l => {
     l.bundles.forEach(b => {
-      b.x = b.parents[0].x + node_width + (l.bundles.length-1-b.i)*bundle_width
+      b.x = b.parents[0].x //+ (l.bundles.length-1-b.i)*bundle_width //+ node_width
       b.y = i*node_height
     })
     i += l.length
@@ -125,6 +132,8 @@ d3.csv("colors.csv").then((colors) => {
     l.xb = l.bundle.x
     l.xs = l.source.x
     l.ys = l.source.y
+    l.c1 = 0 //l.source.level-l.target.level > 1 ? node_width+c : c
+    l.c2 = c
   })
 
   // // compress vertical space
@@ -133,19 +142,18 @@ d3.csv("colors.csv").then((colors) => {
   //   y_negative_offset += -min_family_height + d3.min(l.bundles, b => d3.min(b.links, link => (link.ys-c)-(link.yt+c))) || 0
   //   l.forEach(n => n.y -= y_negative_offset)
   // })
-
-  // very ugly, I know
-  links.forEach(l => {
-    l.yt = l.target.y + l.target.bundles_index[l.bundle.id].i*metro_d - l.target.bundles.length*metro_d/2 + metro_d/2
-    l.ys = l.source.y
-    l.c1 = l.source.level-l.target.level > 1 ? node_width+c : c
-    l.c2 = c
-  })
+  // // very ugly, I know
+  // links.forEach(l => {
+  //   l.yt = l.target.y + l.target.bundles_index[l.bundle.id].i*metro_d - l.target.bundles.length*metro_d/2 + metro_d/2
+  //   l.ys = l.source.y
+  //   l.c1 = 0 //l.source.level-l.target.level > 1 ? node_width+c : c
+  //   l.c2 = c
+  // })
 
   const height = d3.max(nodes, n => n.y) + node_height/2 + 2*padding
   let svg = d3.select('svg')
-    .attr("width", 2500)
-    .attr("height", height)
+    .attr("width", width + margins.left + margins.right)
+    .attr("height", height + margins.top + margins.bottom)
 
   let append_path = (d, color, width) => {
     svg.append("path")
@@ -180,8 +188,9 @@ d3.csv("colors.csv").then((colors) => {
     append_text([xy1[0]+6, xy1[1]-4], text, "black", 0)
   }
 
-  append_line([node_width*5, 0], [node_width*5, height], "gainsboro", 10)
-  append_text([node_width*5 + 10, 20], "การปฏิวัติสยาม", "gainsboro", 0, "gray")
+  const x_2475 = x_scale(2475)
+  append_line([x_2475, 0], [x_2475, height + margins.top + margins.bottom], "gainsboro", 10)
+  append_text([x_2475 + 10, 20], "การปฏิวัติสยาม", "gainsboro", 0, "gray")
 
   bundles.forEach(b => {
     let d = b.links.map(l => `
@@ -192,6 +201,12 @@ d3.csv("colors.csv").then((colors) => {
       A${ l.c2 } ${ l.c2 } 90 0 0 ${ l.xb+l.c2 } ${ l.ys }
       L${ l.xs } ${ l.ys }`
     ).join("");
+
+    // let link = d3.linkHorizontal();
+    // let d = b.links.map(l => link({
+    //   source: [l.xs, l.ys],
+    //   target: [l.xt, l.yt]
+    // })).join("");
 
     append_path(d, "white", 5)
     append_path(d, "black", 2)
